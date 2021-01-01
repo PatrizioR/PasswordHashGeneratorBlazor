278e2e4a78b0556357e1a805333998aef789f1b9
PatrizioR <pricciardello@gmail.com>
2021-01-01 18:29:39 +0000

    Deploying to gh-pages from  @ 8f8e60d7510f731d4c355048d2b9b4ef3c24b4d0 🚀

--- service-worker.js
+++ service-worker.js
@@ -46,4 +46,6 @@ async function onFetch(event) {
     }
 
     return cachedResponse || fetch(event.request);
-}/* Manifest version: YxoFsgQQ */
+}
+
+/* {UPDATED} *//* Manifest version: YxoFsgQQ */
